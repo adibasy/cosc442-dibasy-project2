6a0f710ebc635d67918edcb224dde9b081a020d2
Abdulai <adibas1@students.towson.edu>
2018-02-21 22:16:04 -0500

    Created the empty skeleton for the test

---
+++ Documents/Towson U/Spring 2018/COSC 442/Projects/VendingMachine/src/edu/towson/cis/cosc442/project2/vendingmachine/VendingMachineItemTest.java
@@ -0,0 +1,37 @@
+package edu.towson.cis.cosc442.project2.vendingmachine;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VendingMachineItemTest {
+	
+	VendingMachineItem price1;
+
+	@Before
+	public void setUp() throws Exception {
+		price1 = new VendingMachineItem(null, 0);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testVendingMachineItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPrice() {
+		assertEquals();
+	}
+
+}
